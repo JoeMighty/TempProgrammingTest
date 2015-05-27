bab62611b42ff3cab235db66cba85e67c9eb10a6
JoeMighty <josephwoodward@xeuse.com>
2015-05-27 15:01:47 +0100

    Initial commit

---
+++ Test1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        /// <summary>
+        /// Reverse words appearing within a sentence.
+        /// </summary>
+        /// <param name="str">Sentence with words in original order</param>
+        private static void ReverseWords(string str)
+        {
+            Console.WriteLine("Initial Phrase: {0}", str);
+
+            /* 
+             * Write code to reverse the order of each word in the input phrase (str). For example
+             * 
+             *   red green blue
+             *   
+             * would become:
+             * 
+             *   blue green red
+             * 
+             * You will need to modify the statement below to output the result.
+             */
+
+            Console.WriteLine("Reversed Phrase: {0}", str);
+        }
+
+        static void Main(string[] args)
+        {
+            // You shouldn't need to modify this function.
+            string inputPhrase = "This month I have mostly been eating fish fingers";
+            ReverseWords(inputPhrase);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test1_VB/Program.vb
@@ -0,0 +1,30 @@
+﻿Module Program
+
+    ''' <summary>
+    ''' Reverse words appearing within a sentence.
+    ''' </summary>
+    ''' <param name="str">Sentence with words in original order</param>
+    Private Sub ReverseWords(Str As String)
+        Console.WriteLine("Initial Phrase: {0}", Str)
+
+        ' Write code to reverse the order of each word in the input phrase (str). For example
+        '
+        '     red green blue
+        '
+        ' would become
+        '
+        '     blue green red
+        '
+        ' You will need to modify the statement below to output the result.
+
+        Console.WriteLine("Reversed Phrase: {0}", Str)
+    End Sub
+
+    Sub Main()
+        ' You shouldn't need to modify this function.
+        Dim inputPhrase As String = "This month I have mostly been eating fish fingers"
+        ReverseWords(inputPhrase)
+        Console.ReadKey()
+    End Sub
+
+End Module
